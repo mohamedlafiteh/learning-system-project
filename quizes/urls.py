70fed1dfd6912a9e4415be34baea258553cc9862
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-07-09 13:27:10 +0100

    Fix syle for all pages

--- quizes/urls.py
+++ quizes/urls.py
@@ -10,17 +10,3 @@ urlpatterns = [
     path('<pk>/data/', quiz_data_view, name='quiz-data-view'),
 
 ]
-
-# from django.urls import path
-# from .views import QuizListView,quiz_view,quiz_data_view,save_quiz_view,select_quiz_view
-#
-# app_name ='quizes'
-#
-# urlpatterns = [
-#     path('', QuizListView.as_view(),name='main-view'),
-#     path('select/<fk>/', select_quiz_view, name='select_quiz_view'),
-#     path('select/<lecture_id>/<quiz_id>/',quiz_view,name='quiz-view'),
-#     path('<pk>/save/', save_quiz_view, name='save-view'),
-#     path('<pk>/data/', quiz_data_view, name='quiz-data-view'),
-#
-# ]
