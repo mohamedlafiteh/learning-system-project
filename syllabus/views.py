82747c7bf96e552178156d105ec095546be513c3
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-07-18 17:12:03 +0100

    ref

--- syllabus/views.py
+++ syllabus/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from quizes.models import Quiz
 import datetime
+from quiz.models import Result
 
 
 class LevelView(ListView):
@@ -38,15 +39,26 @@ class LectureView(DetailView):
     model = Subname
     template_name = 'syllabus/lecture_view.html'
 
+
+
+
+
     #This function is used to populate a dictionary to use as the template context
     def get_context_data(self, *args, **kwargs):
         context = super(LectureView, self).get_context_data(**kwargs)
+        u_id=self.request.user.id
+        assess_score = Result.objects.values_list('score', flat=True).filter(user__id=u_id)
+        last_result = None
+        if len(assess_score) > 0:
+            last_result = assess_score.reverse()[len(assess_score) - 1]
+
         goals = self.request.user.user_lecture_goal.all()
         user_goals = []
 
         for goal in goals:
             user_goals.append(goal.lecture.name)
         context['user_goals'] = user_goals
+        context['last_result'] = last_result
         return context
 
     #This function sets or delete the learing goal
